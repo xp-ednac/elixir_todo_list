8d7b26c61f0c498b89458b696461430bdfa14473
Edna Cristina Durans <crisxp841@gmail.com>
2026-01-11 18:41:42 -0300

    Fase 6: Implementa conclusão de tarefas (toggle_complete)

--- lib/elixir_todo_list_web/live/todo_live.ex
+++ lib/elixir_todo_list_web/live/todo_live.ex
@@ -4,66 +4,128 @@ defmodule ElixirTodoListWeb.TodoLive do
   alias ElixirTodoList.Repo
   alias ElixirTodoList.Task
 
-@impl true
-def mount(_params, _session, socket) do
-  tasks = Repo.all(Task)
-  changeset = Task.changeset(%Task{}, %{})
-  form = to_form(changeset)
+  @impl true
+  def mount(_params, _session, socket) do
+    tasks = Repo.all(Task)
+    changeset = Task.changeset(%Task{}, %{})
 
-  socket =
-    assign(socket,
-      tasks: tasks,
-      form: form
-    )
+    {:ok,
+     assign(socket,
+       tasks: tasks,
+       form: to_form(changeset)
+     )}
+  end
 
-  {:ok, socket}
-end
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="w-full max-w-lg mx-auto mt-12 p-6 bg-white rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold mb-6 text-center text-gray-800">
+        Minha Lista de Tarefas (com DB!)
+      </h1>
 
+      <!-- FORMULÁRIO DE NOVA TAREFA -->
+      <.form for={@form} id="task-form" phx-submit="save_task">
+        <.input
+          field={@form[:title]}
+          type="text"
+          label="Nova Tarefa"
+          placeholder="O que precisa ser feito?"
+        />
+        <.button phx-disable-with="Salvando...">
+          Adicionar Tarefa
+        </.button>
+      </.form>
 
-@impl true
-def render(assigns) do
-  ~H"""
-  <div class="w-full max-w-lg mx-auto mt-12 p-6 bg-white rounded-lg shadow-md">
-    <h1 class="text-3xl font-bold mb-6 text-center text-gray-800">Minha Lista de Tarefas (com DB!)</h1>
+      <!-- LISTA DE TAREFAS -->
+      <div class="mt-8">
+        <ul id="task-list">
+          <li
+            :for={task <- @tasks}
+            id={"task-#{task.id}"}
+            class="flex justify-between items-center p-3 border-b"
+          >
 
-    <.form for={@form} id="task-form" phx-submit="save_task">
-      <.input
-        field={@form[:title]}
-        type="text"
-        label="Nova Tarefa"
-        placeholder="O que precisa ser feito?"
-      />
-      <.button phx-disable-with="Salvando...">Adicionar Tarefa</.button>
-    </.form>
+            <% task_form = Task.changeset(task, %{}) |> to_form() %>
 
-    <div class="mt-8">
-      <ul id="task-list">
-        <li :for={task <- @tasks} class="flex justify-between items-center p-3 border-b">
-          <span class={if task.completed, do: "line-through text-gray-500", else: "text-gray-900"}>
-            <%= task.title %>
-          </span>
-        </li>
-      </ul>
+            <!-- CHECKBOX + TÍTULO -->
+            <.form
+              for={task_form}
+              phx-change="toggle_complete"
+              phx-value-id={task.id}
+              class="flex-grow"
+            >
+              <div class="flex items-center space-x-4">
+                <.input
+                  type="checkbox"
+                  field={task_form[:completed]}
+                />
+
+                <label class={
+                  if task.completed,
+                    do: "line-through text-gray-500",
+                    else: "text-gray-900"
+                }>
+                  <%= task.title %>
+                </label>
+              </div>
+            </.form>
+
+            <!-- BOTÃO EXCLUIR -->
+            <.button
+              type="button"
+              phx-click="delete"
+              phx-value-id={task.id}
+              class="!p-1 !bg-red-500 hover:!bg-red-700"
+            >
+              &times;
+            </.button>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-  """
-end
-
+    """
+  end
 
   @impl true
   def handle_event("save_task", %{"task" => params}, socket) do
     case Repo.insert(Task.changeset(%Task{}, params)) do
-    {:ok, _task} ->
-  tasks = Repo.all(Task)
-
-  {:noreply,
-   socket
-   |> assign(:tasks, tasks)
-   |> assign(:form, to_form(Task.changeset(%Task{}, %{})))
-   |> put_flash(:info, "Tarefa salva com sucesso!")}
+      {:ok, _task} ->
+        {:noreply,
+         socket
+         |> assign(:tasks, Repo.all(Task))
+         |> assign(:form, to_form(Task.changeset(%Task{}, %{})))
+         |> put_flash(:info, "Tarefa salva com sucesso!")}
 
       {:error, changeset} ->
         {:noreply, assign(socket, :form, to_form(changeset))}
     end
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    task = Repo.get(Task, id)
+
+    if task do
+      Repo.delete(task)
+    end
+
+    {:noreply,
+     socket
+     |> assign(:tasks, Repo.all(Task))
+     |> put_flash(:info, "Tarefa removida com sucesso!")}
+  end
+
+  @impl true
+  def handle_event("toggle_complete", %{"id" => id, "task" => task_params}, socket) do
+    task = Repo.get!(Task, id)
+
+    completed_status = Map.has_key?(task_params, "completed")
+
+    task
+    |> Task.changeset(%{completed: completed_status})
+    |> Repo.update()
+
+    {:noreply, assign(socket, :tasks, Repo.all(Task))}
   end
 end
